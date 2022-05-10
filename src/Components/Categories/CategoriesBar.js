40e55c42d5466fa28de9f2204ad4788f42ed9632
dudanny712 <prudhviraj0705@gmail.com>
2022-05-10 15:50:51 +0200

    added categories keywords

--- src/Components/Categories/CategoriesBar.js
+++ src/Components/Categories/CategoriesBar.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './_categoriesBar.scss'
-// const keywords = [
-//    'All',
-//    'Batman',
-//    'Superman',
-//    'Flash',
-//    'Marvel',
-//    'DC Comics',
-//    'Dr. Strange',
-//    'Iron Man ',
-//    'Captain America',
-//    'Black Widow',
-//    'Falcon',
-//    'Thor',
-//    'Aquaman',
-//    'Martian',
-//    'Wonder Woman',
-//    'Deadpool',
-//    'X-men',
-// ]
+const keywords = [
+   'All',
+   'Batman',
+   'Superman',
+   'Flash',
+   'Marvel',
+   'DC Comics',
+   'Dr. Strange',
+   'Iron Man ',
+   'Captain America',
+   'Black Widow',
+   'Falcon',
+   'Thor',
+   'Aquaman',
+   'Martian',
+   'Wonder Woman',
+   'Deadpool',
+   'X-men',
+]
 
 const CategoriesBar = () => {
+   const [activeElement, setActiveElement]= useState('All')
+   const handleClick = (value) =>{
+      setActiveElement(value)
+   }
    return (
       <div className='categoriesBar'>
+         {
+            keywords.map((value,index) =><span 
+            onClick={() =>handleClick(value)}
+            className={activeElement=== value? 'active': ''}
+            key={index}>{value}</span>)
+         }
        
       </div>
    )
