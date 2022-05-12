29234ffb9ee7839c061478e2ff98a69d502518c5
dudanny712 <du.danny712@gmail.com>
2022-05-12 12:02:21 +0200

    updated video component

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -8,22 +8,22 @@ const Video = () => {
 
    return (
       <div className='video' >
-         <div className="video_top">
-            <img src="" alt="" />
-            <span>04:05</span>
+         <div className="video__top">
+            <img  src="https://i.ytimg.com/vi/UhKBn3CJA0k/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLBq-Mpn_FxVSz2RhxwV2HtQOY3A3g" alt="" />
+            <span className="video__duration">01:03</span>
          </div>
-         <div className="video_title">
-            Sample title
+         <div className="video__title">
+            Sample title of the video Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni recusandae maiores fuga quas expedita laboriosam, ratione fugiat provident sit repellat delectus et quidem repudiandae iste autem facilis atque nam aliquid?
             
          </div>
-         <div className="video_details">
+         <div className="video__details">
             <span>
-            <AiFillEye/> 12m views
+            <AiFillEye/>  12m views •
             </span>
-            <span>10days ago</span>
+            <span> 10days ago</span>
          </div>
-         <div className="video_channel">
-            <img src="" alt="" />
+         <div className="video__channel">
+            <img src="https://yt3.ggpht.com/ytc/AKedOLTd1D4XGx78xptNiPDa_GhgT01gjnoW4M3qo4rKQQ=s68-c-k-c0x00ffffff-no-rj" alt="" />
             <p>Channel name</p>
          </div>
       </div>
