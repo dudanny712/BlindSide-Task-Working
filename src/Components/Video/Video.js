7bd543e1ea765d2dc0d18ea5d268554f77d601ec
dudanny712 <du.danny712@gmail.com>
2022-05-12 11:44:45 +0200

    added video component

--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { AiFillEye } from 'react-icons/ai'
 import './_video.scss'
 
 
@@ -7,7 +8,24 @@ const Video = () => {
 
    return (
       <div className='video' >
-         Video
+         <div className="video_top">
+            <img src="" alt="" />
+            <span>04:05</span>
+         </div>
+         <div className="video_title">
+            Sample title
+            
+         </div>
+         <div className="video_details">
+            <span>
+            <AiFillEye/> 12m views
+            </span>
+            <span>10days ago</span>
+         </div>
+         <div className="video_channel">
+            <img src="" alt="" />
+            <p>Channel name</p>
+         </div>
       </div>
    )
 }
